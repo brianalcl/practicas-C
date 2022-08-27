8b1df0f392f791376dc107ed379e3b1512c78092
brianalcl <brianclemente@outlook.com>
2022-08-27 10:02:55 -0300

    Se agrego TP1p10

---
+++ TP1p10/palindroma.h
@@ -0,0 +1,8 @@
+#ifndef PALINDROMA_H_INCLUDED
+#define PALINDROMA_H_INCLUDED
+
+int palindroma(char cadena[], int longitud);
+
+int pali(char cadena[], int posIni, int posFin);
+
+#endif // PALINDROMA_H_INCLUDED
