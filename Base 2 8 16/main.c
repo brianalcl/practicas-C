3a83ce431d2ed602748a24d15835df7c989511c1
brianalcl <brianclemente@outlook.com>
2022-09-04 14:52:17 -0300

    Se solucionaron errores en Base 2 8 16

--- Base 2 8 16/main.c
+++ Base 2 8 16/main.c
@@ -14,18 +14,51 @@ long cambioBaseDiezAOtraEntero(long numero, long base){
 
 int main()
 {
+    int aux = 0;
     printf("Intentando ver la relacion entre las bases 2, 8, y 16\n");
-    printf("Veamos los enteros [0, 15] en base 10, 2, 8 y 16\n");
-    printf("+----+----+----+----+\n");
-    printf("|B 10|B 2 |B 8 |B 16|\n");
-    printf("+----+----+----+----+\n");
+    printf("Veamos los enteros [0, 7] en base 10, 2 y 8\n");
+    printf("+---+---+---+\n");
+    printf("|B10|B 2|B 8|\n");
+    printf("+---+---+---+\n");
+    for(long i = 0; i <= 7; i++){
+        printf("|%-3ld|", i);
+        printf("%03ld|", cambioBaseDiezAOtraEntero(i, 2));
+        printf("%3ld|\n", cambioBaseDiezAOtraEntero(i, 8));
+    }
+    printf("+---+---+---+\n");
+    printf("Veamos los enteros [0, 15] en base 10, 2 y 16\n");
+    printf("+----+----+----+\n");
+    printf("|B 10|B 2 |B 16|\n");
+    printf("+----+----+----+\n");
     for(long i = 0; i <= 15; i++){
         printf("|%-4ld|", i);
-        printf("%4ld|", cambioBaseDiezAOtraEntero(i, 2));
-        printf("%-4ld|", cambioBaseDiezAOtraEntero(i, 8));
-        printf("%-4ld|\n", cambioBaseDiezAOtraEntero(i, 16));
+        printf("%04ld|", cambioBaseDiezAOtraEntero(i, 2));
+        aux = cambioBaseDiezAOtraEntero(i, 16);
+        switch (aux) {
+            case 10:
+                printf("   A|\n");
+                break;
+            case 11:
+                printf("   B|\n");
+                break;
+            case 12:
+                printf("   C|\n");
+                break;
+            case 13:
+                printf("   D|\n");
+                break;
+            case 14:
+                printf("   E|\n");
+                break;
+            case 15:
+                printf("   F|\n");
+                break;
+            default:
+                printf("%4ld|\n", aux);
+        }
     }
-    printf("+----+----+----+----+\n");
+
+    printf("+----+----+----+\n");
     printf("-------------------------------------------------------------------------------------------------------\n");
     printf("Cada digito de la base 8 se puede escribir como una secuencia de 3 digitos en base 2\n");
     printf("Para pasar de base 2 a 8 se hace lo siguiente:\n");
