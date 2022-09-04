5399d19ce2fd5fa2837de95d4470af86402f0d26
brianalcl <brianclemente@outlook.com>
2022-09-04 15:23:06 -0300

    Eliminados archivos no deseados

--- TP1p10/palindroma.h
+++
@@ -1,8 +0,0 @@
-#ifndef PALINDROMA_H_INCLUDED
-#define PALINDROMA_H_INCLUDED
-
-int palindroma(char cadena[]);
-
-int pali(char cadena[], int posIni, int posFin);
-
-#endif // PALINDROMA_H_INCLUDED
--- TP2p6/reemplazo.h
+++
@@ -1,7 +0,0 @@
-#ifndef REEMPLAZO_H_INCLUDED
-#define REEMPLAZO_H_INCLUDED
-
-void reemplazar1(int arr[], int a, int n);
-void reemplazar2(int * arr, int a, int n, int size);
-
-#endif // REEMPLAZO_H_INCLUDED
