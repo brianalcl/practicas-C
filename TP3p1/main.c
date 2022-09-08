873b61101341653c7f39b5b5cf5b5702e3e39c17
brianalcl <brianclemente@outlook.com>
2022-09-08 11:41:52 -0300

    Subido TP3p1 por la mitad

---
+++ TP3p1/main.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main()
+{
+    printf("Respuestas en ñ comentarios\n");
+    /*
+    a) ¿Qu´e diferencia una estructura de datos est´atica de una din´amica? Encontrar al
+    menos dos ejemplos de cada categor´ıa.
+
+    La diferencia entre una estructura dinamica y una estatica esta en que:
+
+    la estructura estatica una ves creada no puede ser modificada, y
+    el espacio usado en memoria se mantiene fijo.
+    un ejemplo de estructura estatica es un arreglo, el cual es creado con un tamaño
+    y no puede cambiar.
+
+    la estructura dinamica puede ser modificada, para esto hay que tener
+    en cuenta que el espacio en memoria puede variar.
+    un ejemplo de estructura dinamica es una lista o un arbol que son estructuras
+    que pueden crecer y decrecer.
+
+    b) ¿Una variable de tipo puntero en s´ı misma, constituye una estructura
+    de datos est´atica o din´amica?
+
+    un puntero es una estructura de datos estatica, pues tiene un tamaño fijo el cual no puede
+    ser modificado.
+
+    c) ¿Cu´al es la utilidad de las funciones de librer´ıa malloc() y free()?
+
+    malloc() permite reservar memoria para poder trabajar con estructuras dinamicas
+    a malloc se le pasa por parametro la cantidad de memoria a reservar y luego este
+    retorna un puntero a la zona de memoria reservada.
+    free() permite liberar la memoria previamente asignada.
+    a free se le pasa por parametro un puntero (el que retorno malloc())
+    para liberar la memoria luego free no retorna nada.
+
+    d) ¿C´omo hace el compilador para saber cu´anta memoria debe reservar en el HEAP ante
+    cada invocaci´on a la funci´on malloc()?
+    e) Analice las diferencias que presentan las datos almacenados en STACK, frente aquellos
+    que son almacenados en HEAP. Tenga en cuenta las siguientes caracter´ısticas:
+    Alcance de los datos.
+    Persistencia de los datos: temporal, permanente.
+
+    */
+
+    return 0;
+}
