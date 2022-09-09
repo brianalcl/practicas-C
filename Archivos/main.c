7c8848c089ee63759742ae4aec8e5c18400512f6
brianalcl <brianclemente@outlook.com>
2022-09-09 20:48:43 -0300

    Subida parte 2 de Archivos

--- Archivos/main.c
+++ Archivos/main.c
@@ -14,16 +14,25 @@
     wb, w+b, ab, a+b, rb, r+b.
 
     Escribir en un archivo:
-    fputc() escribe un caracter EJ: fputc('a', archivo);
-    fputs() escribe una cadena de texto EJ: fputs("hola", archivo);
-    fprintf() equivalente a printf
+    fputc('caracter', archivo) escribe un caracter en el archivo.
+    fputs("cadena", archivo) escribe una cadena de texto.
+    fprintf(archivo, "texto con formato", variables) equivalente a printf.
+
+    Leer en un archivo:
+    fgetc(archivo) retorna el caracter en la posicion actual del archivo y avanza.
+    fscanf(archivo, "datos", variables) lee datos del archivo, ejemplo enteros, cadenas, caracteres.
+
+    otras:
+    feof(archivo) retorna verdadero si llegamos al final del archivo.
+    fflush(archivo) limpia el canal de informacion.
+    fclose(archivo) cierra el archivo
 */
 
 int main()
 {
     printf("Intentando trabajar con archivos\n");
     printf("Definamos un archivo en modo escritura:\n");
-    FILE * archivoA = fopen("archivoA.txt", "w"); //el archivo esta alojado en la carpeta raiz
+    FILE * archivoA = fopen("archivoA.txt", "w"); //el archivo esta alojado en la carpeta raiz del proyecto
     printf("Escribamos 'Hola, este es mi primer archivo en C' en el archivo\n");
     printf("Y luego escribamos una matriz de 5x5 con 0's\n");
     if (archivoA != NULL){
@@ -39,7 +48,26 @@ int main()
     printf("Ahora cerramos el archivo\n");
     fflush(archivoA);
     fclose(archivoA);
-
+    printf("-------------------------------------------------------\n");
+    printf("Abramos el archivo y leamoslo:\n");
+    archivoA = fopen("archivoA.txt", "r");
+    char string[100];
+    char caracter = '0';
+    if (archivoA != NULL){
+        if(!feof(archivoA)){
+            fscanf(archivoA, "%s", string);
+            printf(string);
+        }
+        while(!feof(archivoA)){
+            caracter = fgetc(archivoA);
+            printf("%c", caracter);
+        }
+        printf("\r"); //retorno de carro sin bajar de linea.
+    }
+    printf("Veamos que tenemos en la variable string: %s\n", string);
+    printf("Ahora nuevamente cerramos el archivo\n");
+    fflush(archivoA);
+    fclose(archivoA);
     return 0;
 }
 
