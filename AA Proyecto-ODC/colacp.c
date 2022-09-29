266969e9c98ee998959a41ab1ddeb0a02b3bd7b6
brianalcl <brianclemente@outlook.com>
2022-09-29 20:20:16 -0300

    Agregado proyecto ODC

---
+++ AA Proyecto-ODC/colacp.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "colacp.h"
+
+//    https://bit.ly/3y23ZTq
---
+++ AA Proyecto-ODC/colacp.h
@@ -0,0 +1,36 @@
+#ifndef COLACP_H_INCLUDED
+#define COLACP_H_INCLUDED
+
+typedef void *TClave;
+typedef void *TValor;
+
+typedef struct entrada{
+    TClave clave;
+    TValor valor;
+} *TEntrada;
+
+typedef struct nodo{
+    TEntrada entrada;
+    struct nodo *padre;
+    struct nodo *hijo_izquierdo;
+    struct nodo *hijo_derecho;
+} *TNodo;
+
+typedef struct cola_con_prioridad {
+    int cantidad_elementos;
+    TNodo raiz;
+    int (*comparador)(TEntrada, TEntrada);
+} *TColaCP;
+
+TColaCP crear_cola_cp(int (*f)(TEntrada, TEntrada));
+
+int cp_insertar(TColaCP cola, TEntrada entr);
+
+TEntrada cp_eliminar(TColaCP cola);
+
+int cp_cantidad(TColaCP cola);
+
+void cp_destruir(TColaCP cola, void (*fEliminar)(TEntrada));
+
+
+#endif // COLACP_H_INCLUDED
